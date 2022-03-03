6e9e0ebb07f8097ca6c03d22c317bc93569a20ae
choi-seung-won <fortheboost@naver.com>
2022-03-03 16:41:15 +0900

    kh0303

---
+++ Java361/src/day0303/Ex02RealNumber.java
@@ -0,0 +1,18 @@
+package day0303;
+// 기본형 데이터타입 02
+// 실수형 데이터타입
+
+// 실수란?
+// 소숫점이 있는 숫자
+
+// float: 32비트
+// double: 64비트
+
+public class Ex02RealNumber {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ Java361/src/day0303/Ex03Character.java
@@ -0,0 +1,29 @@
+package day0303;
+
+// 기본형 데이터타입 03
+// 문자형 데이터타입
+
+// char은 글자를 뜻하는 character 라는 영어 단어의 줄임말
+
+// 단, char 데이터타입은 더이상 사용 되지 않음.
+// 왜나하면, char 데이터타입의 공간에는 한번에 한개의 글자만 저장 가능하기 때문.
+
+// 따라서 우리는 글자를 저장하거나 표현할 때 char 데이터타입이 아닌
+// String 데이터타입을 사용하여 표현하게 된다.
+
+// 자바 코드 상에서 따옴표와 쌍따옴표의 차이
+// 자바 코드상에서 따옴표는 char 데이터타입의 값을 뜻하며 한번에 한개의 글자만 값에 넣을 수 있다.
+// 그에 반해서 쌍따옴표는 String 데이터타입의 값을 뜻하며 한번에 0개 이상의 글자를 저장할 수 있다.
+
+
+
+public class Ex03Character {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        
+        System.out.println();
+
+    }
+
+}
